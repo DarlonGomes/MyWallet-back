5bff4247982ec4d1a8969b0f707b8d570d9ef918
Darlon Gomes <gomesdarlon@icloud.com>
2022-07-04 13:14:49 -0300

    fix: cors error

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ dotenv.config()
 
 const server = express();
 
-server.use(cors({
-    origin: "https://my-wallet-cochi.vercel.app"
-}));
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 server.use(express.json());
 
 server.use("/user", userHandler);
