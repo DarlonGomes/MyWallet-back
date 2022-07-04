d33053305c387f0f7672b1bc06e38ce91841dd96
Darlon Gomes <gomesdarlon@icloud.com>
2022-07-04 13:15:40 -0300

    fix: cors error

--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ dotenv.config()
 
 const server = express();
 
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+server.use(cors());
 server.use(express.json());
 
 server.use("/user", userHandler);
